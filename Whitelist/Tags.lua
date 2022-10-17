6d252ac9b509bf7879ea2422f22d254f5433a603
Ham-135 <115642877+Ham-135@users.noreply.github.com>
2022-10-17 18:58:37 +0200

    Create Tags.lua

---
+++ Whitelist/Tags.lua
@@ -0,0 +1,10 @@
+return {
+  ["rgfrhfghfgjfgh"] = {
+    TagText = "COMET SCRIPTER",
+    TagColor = Color3.new(255,65,65)
+  },
+  ["nizhggerIPW"] = {
+    TagText = "COMET OWNER",
+    TagColor = Color3.fromRGB(0,255,200)
+  }
+}
