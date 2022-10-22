ec143abd466287bcad16f5c8176fc2ebdcff336b
vxpeprivate <108173427+vxpeprivate@users.noreply.github.com>
2022-10-22 22:23:50 +0700

    Update Bedwars.lua

--- Modules/Bedwars.lua
+++ Modules/Bedwars.lua
@@ -1644,7 +1644,7 @@ runcode(function()
         ["Name"] = "Up",
         ["Function"] = function() end,
         ["Min"] = 0,
-        ["Max"] = 150,
+        ["Max"] = 100,
         ["Default"] = 5,
         ["Round"] = 1
     })
